9901f5036301c34c53c920a0ec6b263852e741e8
Suma Mounica Anumalasetti <sumakrishna1999@gmail.com>
2019-06-10 11:38:39 +0000

    Upload New File

---
+++ Assignment2/rps.py
@@ -0,0 +1,21 @@
+def points(combo: str) -> int:
+    points_dict = {"RP": -1,"RR": 0, "RS": 1, "SS": 0, "SP": 1,"SR": -1,"PR": 1,"PP": 0,"PS": -1}
+    return points_dict[combo]
+
+def comboList(combos: str) -> [str]:
+    return [str(combos).split(",")]
+
+import sys
+no_of_losses = 0
+no_of_wins = 0
+no_of_draws = 0
+for combo in comboList(sys.argv):
+    if points(combo) == 1:
+        no_of_wins += 1
+    elif points(combo) == -1:
+        no_of_wins += -1
+    else:
+        no_of_draws += 1
+print("+" + str(no_of_wins) + "," + "no_of_losses" + "," + str(0) * str( no_of_draws))
+
+
